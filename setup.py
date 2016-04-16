41ffa8ab5df4222b4902f3ff1d1e0b731b4010e2
Lampman Tech <lampmantech@users.noreply.github.com>
2016-04-16 18:20:32 +0300

    made configobj a dependency

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+# -*-mode: python; py-indent-offset: 4; indent-tabs-mode: nil; encoding: utf-8-dos; coding: utf-8 -*-
+
 """
 Behavior-Driven-Development testing for Tryton ERP: http://tryton.org
 
@@ -49,7 +51,7 @@ dParams = dict(name='behaving.trytond',
                namespace_packages=['behaving'],
                include_package_data=True,
                zip_safe=False,
-               install_requires=['setuptools', 'behaving'],
+               install_requires=['behaving', 'configobj'], # sh
                # The code is being developed Unix-only for now - 'Windows', 
                platforms = ['Linux', 'Unix', 'MacOS X'],
                tests_require=['behave', 'proteus', 'trytond'],
@@ -61,7 +63,7 @@ BEHAVE_ARGS="--no-capture --no-capture-stderr --no-skipped --verbose"
 def iMain():
     if sys.argv[-1] == 'publish':
         os.system(sys.executable +" setup.py sdist --formats=gztar upload --show-response")
-#        os.system("python setup.py bdist_wheel upload")
+        os.system(sys.executable +" setup.py bdist_wheel upload --show-response")
         print "You probably want to also tag the version now:"
         print "  git tag -a %s -m 'version %s'" % (version, version, )
         print "  git push --tags"
@@ -73,13 +75,21 @@ def iMain():
             err_msg = e.message.replace("No module named ", "")
             msg = "%s is not installed. Install your test requirments." % err_msg
             raise ImportError(msg)
+        sStartDir = os.path.dirname(os.path.realpath(__file__))
+        sCfgFile = os.path.join(sStartDir, 'src/behaving/tests/features/environment.cfg')
+        assert os.file.exists(sCfgFile), \
+          "You must create and edit the configuration file before you can run the tests\n" \
+          + "Look at the documentation in wiki/Installation.creole\n" \
+          sCfgFile
+          
         from behave.__main__ import main as behave_main
         # We run the behave tests from the src directory
         # so that behaving in on our sys.path, to run from source.
-        sStartDir = os.path.dirname(os.path.realpath(__file__))
         os.chdir(os.path.join(sStartDir, 'src'))
         
         largs = BEHAVE_ARGS.split()
+        # You cannot run all the features recursively.
+        # Look at the documentation in wiki/Installation.creole
         largs.extend(['behaving/tests/features/behave_test.feature'])
         iRetval = behave_main(largs)
         return iRetval
@@ -91,7 +101,8 @@ Extra commands:
   setup.py publish    will sdist upload the distribution to pypi
   setup.py test       will run a very basic test of the software
 """
-    
+        # drop through
+        
     setup(**dParams)
     return 0
 
